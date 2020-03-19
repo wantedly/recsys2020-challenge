812055250201c0b533946139d3e2acfdc9ed1b64
agatan <agatan039@gmail.com>
2020-03-19 10:43:37 +0900

    Fix errors

--- features/base.py
+++ features/base.py
@@ -1,7 +1,7 @@
 import argparse
 import abc
 from typing import Optional, List, Tuple
-from logging import Logger
+from logging import Logger, StreamHandler, INFO, Formatter
 import tempfile
 import os
 
@@ -10,7 +10,7 @@ from google.cloud import storage
 
 
 TESTING = False
-GCS_BUCKET_NAME = "gs://recsys2020-challenge-wantedly"
+GCS_BUCKET_NAME = "recsys2020-challenge-wantedly"
 
 
 class BaseFeature(abc.ABC):
@@ -21,8 +21,13 @@ class BaseFeature(abc.ABC):
         self.name = self.__class__.__name__
         self.debugging = debugging
         self._logger = Logger(self.__class__.__name__)
+        handler = StreamHandler()
+        fmt = Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler.setFormatter(fmt)
+        handler.setLevel(INFO)
+        self._logger.addHandler(handler)
 
-    @abc.abstractproperty
+    @abc.abstractmethod
     def import_columns(self) -> List[str]:
         """この特徴量を作るのに必要なカラムを指定する
         """
@@ -36,17 +41,19 @@ class BaseFeature(abc.ABC):
         """
         ...
 
-    @abc.abstractclassmethod
+    @classmethod
     def add_feature_specific_arguments(cls, parser: argparse.ArgumentParser):
         return
 
     @classmethod
     def main(cls):
+        import logging
+        logging.basicConfig(level=logging.INFO)
         parser = argparse.ArgumentParser()
         parser.add_argument("--debug", action="store_true")
         cls.add_feature_specific_arguments(parser)
         args = parser.parse_args()
-        instance = cls(debugging=args.debugging, **vars(args))
+        instance = cls(debugging=args.debug, **vars(args))
         instance.run()
 
     def run(self):
@@ -56,17 +63,17 @@ class BaseFeature(abc.ABC):
         with tempfile.TemporaryDirectory() as tempdir:
             files: List[str] = []
             if TESTING:
-                test_path = os.path.join(tempdir.name, f"{self.name}_test.ftr")
+                test_path = os.path.join(tempdir, f"{self.name}_test.ftr")
                 test_table = "`wantedly-individual-naomichi.recsys2020.test`"
             else:
-                test_path = os.path.join(tempdir.name, f"{self.name}_val.ftr")
+                test_path = os.path.join(tempdir, f"{self.name}_val.ftr")
                 test_table = "`wantedly-individual-naomichi.recsys2020.val`"
-            train_path = os.path.join(tempdir.name, f"{self.name}_training.ftr")
+            train_path = os.path.join(tempdir, f"{self.name}_training.ftr")
             train_table = "`wantedly-individual-naomichi.recsys2020.training`"
             self.read_and_save_features(
                 train_table, test_table, train_path, test_path,
             )
-            is not self.debugging:
+            if not self.debugging:
                 self._upload_to_gs([test_path, train_path])
 
     def read_and_save_features(
@@ -74,7 +81,7 @@ class BaseFeature(abc.ABC):
         train_table_name: str,
         test_table_name: str,
         train_output_path: str,
-        test_table_name: str,
+        test_output_path: str,
     ) -> None:
         df_train_input = self._read_from_bigquery(train_table_name)
         df_test_input = self._read_from_bigquery(test_table_name)
@@ -87,14 +94,16 @@ class BaseFeature(abc.ABC):
         assert (
             df_test_input.shape[0] == df_test_features.shape[0]
         ), "generated test features is not compatible with the table"
-        self._logger.info(f"Saving features into {output_path}")
         df_train_features.columns = f"{self.name}_" + df_train_features.columns
         df_test_features.columns = f"{self.name}_" + df_test_features.columns
         if self.save_memory:
             # TODO: fp16 にしたりする。
+            self._logger.info("Reduce memory size")
             df_train_features = df_train_features.convert_dtypes()
             df_test_features = df_test_features.convert_dtypes()
+        self._logger.info(f"Saving features to {train_output_path}")
         df_train_features.to_feather(train_output_path)
+        self._logger.info(f"Saving features to {test_output_path}")
         df_test_features.to_feather(test_output_path)
 
     def _read_from_bigquery(self, table_name: str) -> pd.DataFrame:
@@ -104,7 +113,7 @@ class BaseFeature(abc.ABC):
             from {}
             order by tweet_id, engaging_user_id
         """.format(
-            self.import_columns.join(", "), table_name
+            ", ".join(self.import_columns()), table_name
         )
         if self.debugging:
             query += " limit 10000"
@@ -117,6 +126,6 @@ class BaseFeature(abc.ABC):
         bucket = client.get_bucket(GCS_BUCKET_NAME)
         for filename in files:
             basename = os.path.basename(filename)
-            blob = bucket.get_blob(os.path.join("features", basename))
+            blob = storage.Blob(os.path.join("features", basename), bucket)
             self._logger.info(f"Uploading {basename} to {blob.path}")
             blob.upload_from_filename(filename)
