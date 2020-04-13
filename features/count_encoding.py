8da0da84106f88137fb84ffd83d11dcf30e28133
hakubishin3 <shu@wantedly.com>
2020-04-13 15:18:09 +0000

    Fix count encoding (model_23)

--- features/count_encoding.py
+++ features/count_encoding.py
@@ -20,6 +20,16 @@ class CountEncoding(BaseFeature):
             df_train_features[col] = train_result
             df_test_features[col] = test_result
 
+        df_train_input["engaged_engaging"] = df_train_input["engaged_user_id"] + df_train_input["engaging_user_id"]
+        df_test_input["engaged_engaging"] = df_test_input["engaged_user_id"] + df_test_input["engaging_user_id"]
+        for col in ["engaged_engaging"]:
+            train_result, test_result = count_encoding(col, df_train_input, df_test_input)
+            df_train_features[col] = train_result
+            df_test_features[col] = test_result
+
+        print(df_train_features.isnull().sum())
+        print(df_test_features.isnull().sum())
+
         return df_train_features, df_test_features
 
 
