b2edf846a7a9989abb94863c54e2a28511463ed5
hakubishin3 <shu@wantedly.com>
2020-04-15 06:40:00 +0000

    Add model_24

---
+++ features/count_encoding_in_tweetid_units.py
@@ -0,0 +1,95 @@
+import os
+import pandas as pd
+from base import BaseFeature
+from encoding_func import target_encoding
+from google.cloud import storage, bigquery
+from google.cloud import bigquery_storage_v1beta1
+
+
+TESTING = False
+GCS_BUCKET_NAME = "recsys2020-challenge-wantedly"
+PROJECT_ID = "wantedly-individual-naomichi"
+
+
+class CountEncodingInTweetidUnits(BaseFeature):
+    def import_columns(self):
+        return [
+            "tweet_id",
+            "engaging_user_id"
+        ]
+
+    def _read_count_info_from_bigquery(
+            self, train_table_name: str, test_table_name: str, read_table_name: str) -> pd.DataFrame:
+        self._logger.info(f"Reading from {read_table_name}")
+        query = """
+                WITH agg AS (
+                SELECT
+                    engaged_user_id,
+                    count(distinct tweet_id) as n_tweet,
+                    sum(n_engagement) / count(distinct tweet_id) as n_engagement_per_tweet
+                FROM (
+                    (
+                        SELECT tweet_id, any_value(engaged_user_id) as engaged_user_id, count(*) as n_engagement
+                        FROM {}
+                        GROUP BY tweet_id
+                    )
+                    UNION ALL
+                    (
+                        SELECT tweet_id, any_value(engaged_user_id) as engaged_user_id, count(*) as n_engagement
+                        FROM {}
+                        GROUP BY tweet_id
+                    )
+                )
+                GROUP BY
+                    engaged_user_id
+                )
+                SELECT
+                -- t0.engaged_user_id,
+                f1.n_tweet,
+                f1.n_engagement_per_tweet,
+                FROM {} AS t0
+                LEFT OUTER JOIN agg AS f1
+                ON t0.engaged_user_id = f1.engaged_user_id
+                ORDER BY t0.tweet_id, t0.engaging_user_id
+        """.format(train_table_name, test_table_name, read_table_name)
+        if self.debugging:
+            query += " limit 10000"
+
+        bqclient = bigquery.Client(project=PROJECT_ID)
+        bqstorageclient = bigquery_storage_v1beta1.BigQueryStorageClient()
+        df = (
+            bqclient.query(query)
+            .result()
+            .to_dataframe(bqstorage_client=bqstorageclient)
+        )
+        return df
+
+    def make_features(self, df_train_input, df_test_input):
+        train_table = f"`{PROJECT_ID}.recsys2020.training`"
+        if TESTING:
+            test_table = f"`{PROJECT_ID}.recsys2020.test`"
+        else:
+            test_table = f"`{PROJECT_ID}.recsys2020.val`"
+        train_info = self._read_count_info_from_bigquery(train_table, test_table, train_table)
+        test_info = self._read_count_info_from_bigquery(train_table, test_table, test_table)
+        feature_names = ["n_tweet", "n_engagement_per_tweet"]
+        print(train_info.shape)
+        print(train_info.isnull().sum())
+        print(test_info.shape)
+        print(test_info.isnull().sum())
+
+        df_train_features = pd.DataFrame()
+        df_test_features = pd.DataFrame()
+
+        for fe in feature_names:
+            df_train_features[fe] = train_info[fe].values
+            df_test_features[fe] = test_info[fe].values
+
+        print(df_train_features.isnull().sum())
+        print(df_test_features.isnull().sum())
+
+        return df_train_features, df_test_features
+
+
+if __name__ == "__main__":
+    CountEncodingInTweetidUnits.main()
