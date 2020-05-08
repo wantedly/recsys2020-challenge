61cdaa47865d552a2524ff2f77c3629187f6ff30
agatan <agatan039@gmail.com>
2020-05-08 09:56:18 +0000

    Compute personalized pagerank

--- features/like_graph.py
+++ features/like_graph.py
@@ -1,10 +1,10 @@
+from collections import defaultdict
+import itertools
+
 import pandas as pd
 import numpy as np
 import networkx as nx
 import scipy.sparse as sparse
-import dgl
-import dgl.function as fn
-import torch
 import tqdm
 
 from base import BaseFeature
@@ -22,11 +22,13 @@ class LikeGraph(BaseFeature):
         df_train_features = pd.DataFrame({
             "engager_like_out_degree": np.zeros(len(df_train_input), dtype=np.float32),
             "engaged_like_in_degree": np.zeros(len(df_train_input), dtype=np.float32),
+            "like_personalized_pagerank": np.zeros(len(df_train_input), dtype=np.float32),
         })
         df_test_features = [
             pd.DataFrame({
                 "engager_like_out_degree": np.zeros(len(df_test_input), dtype=np.float32),
                 "engaged_like_in_degree": np.zeros(len(df_test_input), dtype=np.float32),
+                "like_personalized_pagerank": np.zeros(len(df_test_input), dtype=np.float32),
             })
             for _ in range(len(df_folds.unique()))
         ]
@@ -48,9 +50,46 @@ class LikeGraph(BaseFeature):
             ]
             graph = self.G.copy()
             graph.remove_edges_from(edges)
+
+            ## Personalized PageRank
+            walk_graph = graph.to_directed(as_view=True)
+            ppl_cache = {}
+            ppls = defaultdict(lambda: 0)
+            for engaging_user_id, engaged_user_id in tqdm.tqdm(itertools.chain(
+                zip(df.engaging_user_id.values, df.engaged_user_id.values),
+                zip(df_test_input.engaging_user_id.values, df_test_input.engaged_user_id.values),
+            ), total=len(df) + len(df_test_input), desc="ppl"):
+                engaging_node_id = self.user2id.get(engaging_user_id)
+                engaged_node_id = self.user2id.get(engaged_user_id)
+                if engaging_node_id is None or engaged_node_id is None:
+                    continue
+                p = ppl_cache.get(engaging_user_id)
+                if p is None:
+                    steps = 100
+                    jumpback = np.random.random(steps) < 0.15
+                    counter = defaultdict(lambda: 0)
+                    last_node = engaging_node_id
+                    for step in range(steps):
+                        counter[last_node] += 1
+                        if jumpback[step]:
+                            last_node = engaging_node_id
+                            continue
+                        successors = list(graph.successors(last_node))
+                        if not successors:
+                            last_node = engaging_node_id
+                        else:
+                            last_node = successors[np.random.randint(len(successors))]
+                    p = counter
+                    ppl_cache[engaging_user_id] = p
+                ppls[(engaging_user_id, engaged_user_id)] = p.get(engaged_node_id, 0.0)
+            df_train_features.loc[df_folds == fold, "like_personalized_pagerank"] = \
+                df_train_input.loc[df_folds == fold, ["engaging_user_id", "engaged_user_id"]].apply(lambda r: ppls.get((r.engaging_user_id, r.engaged_user_id), 0.0), axis=1)
+            df_test_features[i].loc[:, "like_personalized_pagerank"] = \
+                df_test_input.loc[:, ["engaging_user_id", "engaged_user_id"]].apply(lambda r: ppls.get((r.engaging_user_id, r.engaged_user_id), 0.0), axis=1)
+
+
             engaging_user_ids = set(df.engaging_user_id.unique()).union(df_test_input.engaging_user_id.unique())
             engaged_user_ids = set(df.engaged_user_id.unique()).union(df_test_input.engaged_user_id.unique())
-
             engaging_like_out_degrees = {}
             for engaging_user_id in tqdm.tqdm(engaging_user_ids, desc=f"fold {fold} (engaging)"):
                 engaging_user_id_int = self.user2id.get(engaging_user_id)
