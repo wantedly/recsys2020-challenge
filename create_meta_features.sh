c471a9e1346ddc6316e2dc222ffe6d743d8a6f25
hakubishin3 <shu@wantedly.com>
2020-05-24 14:38:47 +0000

    Add bert and meta

---
+++ create_meta_features.sh
@@ -0,0 +1,5 @@
+python -u features/meta_features.py --debug
+python -u features/meta_features_agg_by_engaged_user_id.py --debug
+python -u features/meta_features_agg_by_engaging_user_id.py --debug
+python -u features/meta_features_agg_by_tweet_id.py --debug
+
