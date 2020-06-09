0babb5ae6c368b75204cf085dccc9353f908a3c1
agatan <agatan039@gmail.com>
2020-06-09 09:39:54 +0000

    Remove unused preprocessing code

--- model_lgb_hakubishin_20200317/run_multi_output.py
+++ model_lgb_hakubishin_20200317/run_multi_output.py
@@ -99,55 +99,6 @@ def main():
     logger.debug(f'x_test: {x_test.shape}')
     logger.debug(f'key_test: {key_test.shape}')
 
-    # =========================================
-    # === Feature processing
-    # =========================================
-    def rank_gauss(x):
-        from scipy.special import erfinv
-        N = x.shape[0]
-        temp = x.argsort()
-        rank_x = temp.argsort() / N
-        rank_x -= rank_x.mean()
-        rank_x *= 2
-        efi_x = erfinv(rank_x)
-        efi_x -= efi_x.mean()
-        return efi_x
-
-    x_total = x_train.append(x_test).reset_index(drop=True)
-
-    # add na flg
-    for col in x_total.columns:
-        x_total[f"NAFlg_{col}"] = x_total[col].isnull().astype(int)
-
-    # one-hot encoding
-    x_total = pd.get_dummies(x_total, columns=["LabelEncoding"])
-
-    # fillna
-    x_total = x_total.fillna(x_total.mean())
-    logger.debug(f"the number of na: {x_total.isnull().sum().sum()}")
-
-    # normalize
-    not_numeric_feature_classes = [
-        "NAFlg",
-        "LabelEncoding",
-        "CommonFlgFeatures",
-        "EngagingUserFollowsEngagedUser",
-        "Connected2ndEngagingToEngaged",
-        "Connected2ndEngagedToEngaging"
-    ]
-    not_numeric_features = []
-    for not_numeric in not_numeric_feature_classes:
-        not_numeric_features += [c for c in x_total.columns if c.find(not_numeric + "_") != -1]
-    logger.debug(f"not_numeric_features: {len(not_numeric_features)}")
-
-    numeric_features = [c for c in x_total.columns if c not in not_numeric_features]
-    logger.debug(f"numeric_features: {len(numeric_features)}")
-
-    for numeric in numeric_features:
-        x_total[numeric] = rank_gauss(x_total[numeric].values)
-
-    x_train = x_total.iloc[:len(y_train_set)]
-    x_test = x_total.iloc[len(y_train_set):].reset_index(drop=True)
     logger.debug(f'number of features in train: {x_train.shape}')
     logger.debug(f'number of features in test: {x_test.shape}')
 
@@ -159,14 +110,16 @@ def main():
 
     # Modeling
     target_columns = [
-        "reply_engagement",
-        "retweet_engagement",
-        "retweet_with_comment_engagement",
-        "like_engagement",
+        'TargetCategories_reply_engagement',
+        'TargetCategories_retweet_engagement',
+        'TargetCategories_retweet_with_comment_engagement',
+        'TargetCategories_like_engagement',
     ]
 
     # Get target values
-    y_train = y_train_set[target_columns].values
+    print(y_train_set.columns)
+    y_train = y_train_set[target_columns].values.astype(np.float32)
+    print(y_train.shape, y_train.dtype, y_train)
 
     # Get folds
     folds_col = ["StratifiedGroupKFold_retweet_with_comment_engagement"]
